56ff90757792a5a161b6ad2f92e8ff61e6c23fb8
mamzaw <gmohammadreza867@gmail.com>
2024-12-19 23:28:47 +0330

    Fix (Card) = Fixing Card & Navbar Style

--- front-end/src/components/CardsSection/Card.tsx
+++ front-end/src/components/CardsSection/Card.tsx
@@ -13,7 +13,7 @@ const CardsSection = () => {
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
                 {/* Card 1 */}
-                <div className="bg-red-100 rounded-lg shadow-md p-4 text-center">
+                <div className="bg-whiten rounded-lg shadow-md p-4 text-center">
                     <img
                         src={Product}
                         alt="Product 1"
@@ -28,7 +28,7 @@ const CardsSection = () => {
                 </div>
 
                 {/* Card 2 */}
-                <div className="bg-red-100 rounded-lg shadow-md p-4 text-center">
+                <div className="bg-whiten rounded-lg shadow-md p-4 text-center">
                     <img
                         src={Product}
                         alt="Product 2"
@@ -43,7 +43,7 @@ const CardsSection = () => {
                 </div>
 
                 {/* Card 3 */}
-                <div className="bg-red-100 rounded-lg shadow-md p-4 text-center">
+                <div className="bg-whiten rounded-lg shadow-md p-4 text-center">
                     <img
                         src={Product}
                         alt="Product 3"
@@ -58,7 +58,63 @@ const CardsSection = () => {
                 </div>
 
                 {/* Card 4 */}
-                <div className="bg-red-100 rounded-lg shadow-md p-4 text-center">
+                <div className="bg-whiten rounded-lg shadow-md p-4 text-center">
+                    <img
+                        src={Product}
+                        alt="Product 4"
+                        className="mx-auto w-32 h-32 object-cover"
+                    />
+                    <h3 className="text-lg font-semibold mt-4 text-gray-800">
+                        محصول چهارم
+                    </h3>
+                    <p className="text-gray-600 mt-2 text-sm">
+                        توضیحات مختصر برای این محصول
+                    </p>
+                </div>
+                {/* Card 5 */}
+                <div className="bg-whiten rounded-lg shadow-md p-4 text-center">
+                    <img
+                        src={Product}
+                        alt="Product 4"
+                        className="mx-auto w-32 h-32 object-cover"
+                    />
+                    <h3 className="text-lg font-semibold mt-4 text-gray-800">
+                        محصول چهارم
+                    </h3>
+                    <p className="text-gray-600 mt-2 text-sm">
+                        توضیحات مختصر برای این محصول
+                    </p>
+                </div>
+                {/* Card 6 */}
+                <div className="bg-whiten rounded-lg shadow-md p-4 text-center">
+                    <img
+                        src={Product}
+                        alt="Product 4"
+                        className="mx-auto w-32 h-32 object-cover"
+                    />
+                    <h3 className="text-lg font-semibold mt-4 text-gray-800">
+                        محصول چهارم
+                    </h3>
+                    <p className="text-gray-600 mt-2 text-sm">
+                        توضیحات مختصر برای این محصول
+                    </p>
+                </div>
+                {/* Card 7 */}
+                <div className="bg-whiten rounded-lg shadow-md p-4 text-center">
+                    <img
+                        src={Product}
+                        alt="Product 4"
+                        className="mx-auto w-32 h-32 object-cover"
+                    />
+                    <h3 className="text-lg font-semibold mt-4 text-gray-800">
+                        محصول چهارم
+                    </h3>
+                    <p className="text-gray-600 mt-2 text-sm">
+                        توضیحات مختصر برای این محصول
+                    </p>
+                </div>
+                {/* Card 8 */}
+                <div className="bg-whiten rounded-lg shadow-md p-4 text-center">
                     <img
                         src={Product}
                         alt="Product 4"
@@ -119,6 +175,54 @@ const CardsSection = () => {
                             className="mx-auto rounded-full w-20 h-20"
                         />
                         <p className="mt-2 text-gray-600">کالای دیجیتال</p>
+                    </div>
+                    <div className="text-center">
+                        <img
+                            src={Organic}
+                            alt="Category 6"
+                            className="mx-auto rounded-full w-20 h-20"
+                        />
+                        <p className="mt-2 text-gray-600">خودرو و سفر</p>
+                    </div>
+                    <div className="text-center">
+                        <img
+                            src={Organic}
+                            alt="Category 6"
+                            className="mx-auto rounded-full w-20 h-20"
+                        />
+                        <p className="mt-2 text-gray-600">خودرو و سفر</p>
+                    </div>
+                    <div className="text-center">
+                        <img
+                            src={Organic}
+                            alt="Category 6"
+                            className="mx-auto rounded-full w-20 h-20"
+                        />
+                        <p className="mt-2 text-gray-600">خودرو و سفر</p>
+                    </div>
+                    <div className="text-center">
+                        <img
+                            src={Organic}
+                            alt="Category 6"
+                            className="mx-auto rounded-full w-20 h-20"
+                        />
+                        <p className="mt-2 text-gray-600">خودرو و سفر</p>
+                    </div>
+                    <div className="text-center">
+                        <img
+                            src={Organic}
+                            alt="Category 6"
+                            className="mx-auto rounded-full w-20 h-20"
+                        />
+                        <p className="mt-2 text-gray-600">خودرو و سفر</p>
+                    </div>
+                    <div className="text-center">
+                        <img
+                            src={Organic}
+                            alt="Category 6"
+                            className="mx-auto rounded-full w-20 h-20"
+                        />
+                        <p className="mt-2 text-gray-600">خودرو و سفر</p>
                     </div>
                     <div className="text-center">
                         <img
--- front-end/src/components/Navbar/Navbar.tsx
+++ front-end/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,6 @@
 import {useNavigate} from "react-router-dom";
+import Avatar from '../../images/thunder.jpg'
+
 const Navbar = () => {
     const navigate = useNavigate();
     return (
@@ -6,19 +8,21 @@ const Navbar = () => {
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-end py-3">
                 {/* Logo Section */}
                 <div className="flex items-center space-x-4">
-                    <a href="/" className="text-red-500 text-xl font-bold ml-4">
-                        Thunder-dev
-                    </a>
+                    <img
+                        src={Avatar}
+                        alt="Category 6"
+                        className=" rounded-full w-15 h-15"
+                    />
                 </div>
                 {/* Search Bar */}
-                <div className="flex-1 mx-6 text-right">
+                <div className="flex-1 mx-20">
                     <div className="relative">
                         <input
                             type="text"
-                            placeholder="Search for products, brands, and more..."
-                            className="w-full py-2 px-4 bg-blend-color:black border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-red-300 text-right"
+                            placeholder="جستجو..."
+                            className="w-1/2 py-2 px-10 bg-whiten text-white border border-b-whiter rounded-full focus:outline-none focus:ring-1 focus:ring-gray-600 text-right"
                         />
-                        <button className="absolute right-3 top-2 text-gray-500 hover:text-red-500">
+                        <button className="relative left-6 top-1 text-gray-500 hover:text-red-500">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
@@ -40,14 +44,15 @@ const Navbar = () => {
                 {/* Icons and Navigation */}
                 <div className="flex items-center space-x-4">
                     {/* Login/Signup Button */}
-                    <div className="flex items-center border border-gray-300 rounded-md px-4 py-2 space-x-2 hover:shadow-sm cursor-pointer">
+                    <div
+                        className="flex items-center border border-gray-300 rounded-md px-4 py-2 mx-5.5 space-x-2 hover:shadow-6 bg-gray cursor-pointer">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
                             viewBox="0 0 24 24"
                             strokeWidth="1.5"
                             stroke="currentColor"
-                            className="w-6 h-6"
+                            className="w-5 h-5"
                         >
                             <path
                                 strokeLinecap="round"
@@ -55,9 +60,15 @@ const Navbar = () => {
                                 d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
                             />
                         </svg>
-                        <button onClick={ ()=>navigate('auth/signin') } className="text-gray-700 text-sm font-medium hover:text-red-500">
+                        <button onClick={() => navigate('auth/signin')}
+                                className="text-gray-700 text-sm font-medium hover:text-red-500">
                             ورود | ثبت ‌نام
                         </button>
+                    </div>
+                    <div className="font-bold text-center font-medium">
+                        <p>
+                            |
+                        </p>
                     </div>
                     {/* Shopping Cart Button */}
                     <button className="relative text-gray-700 hover:text-red-500">
