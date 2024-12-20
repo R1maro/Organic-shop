12c9484a8a28b22521e5dc37779289e064db46eb
R1maro <amir.mmdi1221@gmail.com>
2024-12-21 01:17:40 +0330

    fix(Product) = product image show

--- front-end/src/config/index.ts
+++ front-end/src/config/index.ts
@@ -1,14 +1,17 @@
 interface Config {
     API_URL: string;
+    PUBLIC_URL:string;
     APP_NAME: string;
 }
 
 
 const apiUrl = import.meta.env.VITE_APP_URL || 'http://localhost:8000/api';
+const publicUrl = import.meta.env.VITE_PUBLIC_URL || 'http://localhost:8000';
 
 
 const config: Config = {
     API_URL: apiUrl,
+    PUBLIC_URL: publicUrl,
     APP_NAME: 'Organic-shop',
 };
 
