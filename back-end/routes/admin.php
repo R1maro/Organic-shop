101a8c3f6cfd9f3c1dca4b7354baebe6ff38214f
R1maro <amir.mmdi1221@gmail.com>
2025-01-19 00:06:07 +0330

    feat(Users) = add UserController with ApiResource and routes

--- back-end/routes/admin.php
+++ back-end/routes/admin.php
@@ -5,10 +5,15 @@ use App\Http\Controllers\Dashboard\InvoiceController;
 use App\Http\Controllers\Dashboard\OrderController;
 use App\Http\Controllers\Dashboard\ProductController;
 use App\Http\Controllers\Dashboard\SettingController;
+use App\Http\Controllers\Dashboard\UserController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('admin')->group(function () {
+    Route::apiResource('users', UserController::class);
+    Route::get('users/search', [UserController::class, 'search']);
+    Route::get('users/trashed', [UserController::class, 'trashed']);
+    Route::post('users/{id}/restore', [UserController::class, 'restore']);
     Route::apiResource('categories', CategoryController::class);
     Route::apiResource('products', ProductController::class);
     Route::apiResource('orders', OrderController::class);
