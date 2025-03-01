925ebe2b1fb7be3ab58487f6448e23a7ba2e268e
R1maro <amir.mmdi1221@gmail.com>
2025-03-01 13:53:53 +0330

    fix(Carousel) = autoplay add

--- front-end/src/components/Carousel/3D-Carousel.tsx
+++ front-end/src/components/Carousel/3D-Carousel.tsx
@@ -1,10 +1,12 @@
 'use client';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {CarouselProps} from "@/types/carousel";
 import Slide from "@/components/Carousel/Slide";
 
 const Carousel: React.FC<CarouselProps> = ({slides, isPageBackground}) => {
     const [slideIndex, setSlideIndex] = useState(0);
+    const [isPaused, setIsPaused] = useState(false);
+    const autoplayInterval = 3000;
 
     const handlePrevSlide = () => {
         setSlideIndex((prev) => (prev === 0 ? slides.length - 1 : prev - 1));
@@ -14,8 +16,19 @@ const Carousel: React.FC<CarouselProps> = ({slides, isPageBackground}) => {
         setSlideIndex((prev) => (prev + 1) % slides.length);
     };
 
+    useEffect(() => {
+        if (isPaused) return; // Stop autoplay when paused
+
+        const interval = setInterval(handleNextSlide, autoplayInterval);
+        return () => clearInterval(interval); // Cleanup on unmount
+    }, [slideIndex, isPaused]);
+
     return (
-        <section className="slidesWrapper">
+        <section
+            className="slidesWrapper"
+            onMouseEnter={() => setIsPaused(true)}
+            onMouseLeave={() => setIsPaused(false)}
+        >
             <div className="slides">
 
                 <button className="prevSlideBtn" onClick={handlePrevSlide}>
@@ -26,15 +39,15 @@ const Carousel: React.FC<CarouselProps> = ({slides, isPageBackground}) => {
                     let offset = slides.length + (slideIndex - i);
 
                     return (
-                            <Slide
-                                key={`slide-${i}`}
-                                image={slide.image}
-                                label={slide.label}
-                                subtitle={slide.subtitle}
-                                description={slide.description}
-                                offset={offset}
-                                isPageBackground={isPageBackground}
-                            />
+                        <Slide
+                            key={`slide-${i}`}
+                            image={slide.image}
+                            label={slide.label}
+                            subtitle={slide.subtitle}
+                            description={slide.description}
+                            offset={offset}
+                            isPageBackground={isPageBackground}
+                        />
                     );
                 })}
                 <button className="nextSlideBtn" onClick={handleNextSlide}>
