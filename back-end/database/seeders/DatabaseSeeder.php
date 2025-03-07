39f3e92995c90d25b59d2cb664cf363e2b412f98
R1maro <amir.mmdi1221@gmail.com>
2025-03-07 11:49:01 +0330

    fix(seeder)

--- back-end/database/seeders/DatabaseSeeder.php
+++ back-end/database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
         ]);
         $this->call(SettingSeeder::class);
         $this->call(RoleSeeder::class);
-        $this->call(ProductSeeder::class);
         $this->call(CategorySeeder::class);
+        $this->call(ProductSeeder::class);
     }
 }
