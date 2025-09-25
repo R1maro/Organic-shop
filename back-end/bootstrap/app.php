87872701df4cb86c730fdbdcac3273bc2c8260f8
R1maro <amir.mmdi1221@gmail.com>
2025-09-25 12:44:58 +0330

    fix(cors)

--- back-end/bootstrap/app.php
+++ back-end/bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Fruitcake\Cors\HandleCors::class,
         ]);
 
         $middleware->alias([
